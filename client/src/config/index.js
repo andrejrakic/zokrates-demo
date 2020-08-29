b6a334fa092fc13748fbb76ebfbb039403ea2240
andrejrakic <andrej.rakic@elfak.rs>
2020-08-29 15:06:27 +0200

    code cleanup

--- client/src/config/index.js
+++ client/src/config/index.js
@@ -1,17 +1,20 @@
 import Web3 from 'web3';
 import verifier_abi from '../contracts/verifierABI.sol.json';
 
+const verifierContractAddress = `0x600c3dC2568d97D85082bE1110f7a9dC52C2123C`;
+const infuraId = `fcba35ab93d548cba21391640fcb4c2b`;
+
 export const serverUri = `http://localhost:5000`;
 
 export const web3 = new Web3(
 	new Web3.providers.WebsocketProvider(
-		`wss://rinkeby.infura.io/ws/v3/fcba35ab93d548cba21391640fcb4c2b`
+		`wss://rinkeby.infura.io/ws/v3/${infuraId}`
 	)
 );
 
 export const VerifierContract = new web3.eth.Contract(
 	verifier_abi,
-	`0x600c3dC2568d97D85082bE1110f7a9dC52C2123C`
+	verifierContractAddress
 );
 
 export const testWeb3Connection = () => {
